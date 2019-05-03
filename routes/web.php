7e9eaa8dbda1658c2a98a2335de286d3d54529da
Victor Gonzalez <vicgonvt@gmail.com>
2019-05-03 18:14:19 -0400

    adds basic test for book

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/books', 'BooksController@store');
+Route::patch('/books/{book}', 'BooksController@update');
---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Book;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Victor',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+        $response = $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => 'Victor',
+        ]);
+
+        $book = Book::first();
+
+        $response = $this->patch('/books/' . $book->id, [
+            'title' => 'New Title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
+    }
+}
