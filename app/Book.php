35e28acd46c92a62135bc427ca7ae4c509571859
Victor Gonzalez <vicgonvt@gmail.com>
2019-05-05 22:34:55 -0400

    adds new path to books resource

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/' . $this->id;
+    }
 }
